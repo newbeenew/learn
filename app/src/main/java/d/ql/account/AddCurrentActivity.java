8de9ec2f873f05d43fbd4975a81cea43d45b958e
ql <winlun@126.com>
2016-03-10 22:40:07 +0800

    add textbox

--- app/src/main/java/d/ql/account/AddCurrentActivity.java
+++ app/src/main/java/d/ql/account/AddCurrentActivity.java
@@ -16,21 +16,17 @@ public class AddCurrentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_current);
-        EditText editText = (EditText) findViewById(R.id.current_input);
-        editText.setInputType(EditorInfo.TYPE_CLASS_PHONE);
-
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask()
                        {
                            public void run()
-
                            {
                                EditText editText = (EditText) findViewById(R.id.current_input);
+                               editText.setInputType(EditorInfo.TYPE_CLASS_NUMBER);
                                InputMethodManager inputManager =
-
-                                       (InputMethodManager)editText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-
+                                       (InputMethodManager)editText.getContext().
+                                               getSystemService(Context.INPUT_METHOD_SERVICE);
                                inputManager.showSoftInput(editText, 0);
 
                            }
